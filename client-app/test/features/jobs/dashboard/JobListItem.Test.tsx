3e933dc23a1e542b0aa92656c28614301cf59d41
Shahar Lazarovich <shaharlazarovich@gmail.com>
2020-01-12 08:29:38 +0200

    passign job as object to unitests
    Signed-off-by: Shahar Lazarovich <shaharlazarovich@gmail.com>

--- client-app/test/features/jobs/dashboard/JobListItem.Test.tsx
+++ client-app/test/features/jobs/dashboard/JobListItem.Test.tsx
@@ -1,18 +1,30 @@
 import { shallow } from "enzyme";
 import React from "react";
 import { findByTestAttr } from '../../../common/testUtils';
-import { IJob } from '../../../../src/app/models/job'
+import { IJob, JobFormValues } from '../../../../src/app/models/job'
 import JobListItem from "../../../../src/features/jobs/dashboard/JobListItem";
 
 const setup = (job:any) => {
-    const wrapper = shallow<IJob>(<JobListItem {...job} />)
+    const wrapper = shallow<IJob>(<JobListItem job={job} />)
     return wrapper;
 }
 
 describe('Job List Item UniTest', () => {
     let wrapper:any;
+    let testJob = new JobFormValues();
     beforeEach(() => {
-        wrapper = setup([]);
+        testJob = { 
+            id:"4", 
+            replication:"VMware SRM",
+            jobName:"test1",
+            company: 'Shomera',  
+            lastRun: new Date('2020-01-06 23:00:05'),  
+            servers: '10',
+            results: 'OK',
+            key: 'AAAA-BBBB-CCCC-DDDD',
+            rtoNeeded: 10
+        }
+        wrapper = setup({ testJob });
     });
     test('renders without error', () => {
         const component = findByTestAttr(wrapper, 'component-job-list-item');
--- client-app/test/features/jobs/details/JobDetailedHeader.Test.tsx
+++ client-app/test/features/jobs/details/JobDetailedHeader.Test.tsx
@@ -1,18 +1,30 @@
 import { shallow } from "enzyme";
 import React from "react";
-import { IJob } from '../../../../src/app/models/job'
+import { IJob, JobFormValues } from '../../../../src/app/models/job'
 import { findByTestAttr } from '../../../common/testUtils';
 import JobDetailedHeader from "../../../../src/features/jobs/details/JobDetailedHeader";
 
 const setup = (job:any) => {
-    const wrapper = shallow<IJob>(<JobDetailedHeader {...job} />)
+    const wrapper = shallow<IJob>(<JobDetailedHeader job={job} />)
     return wrapper;
 }
 
 describe('Job Detailed Header UniTest', () => {
     let wrapper:any;
+    let testJob = new JobFormValues();
     beforeEach(() => {
-        wrapper = setup([]);
+      testJob = { 
+            id:"4", 
+            replication:"VMware SRM",
+            jobName:"test1",
+            company: 'Shomera',  
+            lastRun: new Date('2020-01-06 23:00:05'),  
+            servers: '10',
+            results: 'OK',
+            key: 'AAAA-BBBB-CCCC-DDDD',
+            rtoNeeded: 10
+      }
+      wrapper = setup({ testJob });
     });
     test('renders without error', () => {
         const component = findByTestAttr(wrapper, 'component-job-detailed-header');
